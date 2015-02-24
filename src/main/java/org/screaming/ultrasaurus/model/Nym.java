83e67aae23db24094711f5ce495739dabbcd3939
Pohl Longsine <pohl_longsine@gallup.com>
2015-02-24 11:40:17 -0600

    uri encoding fragments

--- src/main/java/org/screaming/ultrasaurus/model/Nym.java
+++ src/main/java/org/screaming/ultrasaurus/model/Nym.java
@@ -1,15 +1,20 @@
 package org.screaming.ultrasaurus.model;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * @author pohl_longsine
  */
 public class Nym {
     
     private final String entry;
+    private final String uriEncodedEntry;
     private final boolean isLinkable;
     
     public Nym(String entry) {
         this.entry = entry;
+        this.uriEncodedEntry = uriEncode(entry);
         isLinkable = isLinkable(entry);
     }
 
@@ -19,6 +24,10 @@ public class Nym {
 
     public boolean isLinkable() {
         return isLinkable;
+    }
+
+    public String getUriEncodedEntry() {
+        return uriEncodedEntry;
     }
 
     private static final boolean isLinkable(CharSequence seq) {
@@ -33,4 +42,17 @@ public class Nym {
         }
         return true;
     }
+
+
+    private static final String uriEncode(String entry) {
+        try {
+            return URLEncoder.encode(entry, "UTF-8")
+                    .replaceAll("\\+", "%20")
+                    .replaceAll("\\%27", "'");
+        } catch (UnsupportedEncodingException e) {
+            return entry;
+        }
+    }
+
+
 }
